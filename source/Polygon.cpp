d420cd4d6945e1b6ddb90fb9d5ef9d13475c2b82
Craig McCulloch <pyridoxus@null.net>
2011-11-26 14:43:10 -0600

    Able to split the face strings into the appropriate arrays for each polygon.

--- source/Polygon.cpp
+++ source/Polygon.cpp
@@ -1,4 +1,5 @@
 #include "Polygon.h"
+#include <stdlib.h>
 
 Polygon::Polygon(String face)
 {
@@ -19,12 +20,22 @@ Polygon::~Polygon()
 
 void Polygon::setPolygon(String face)
 {
-	String p1, p2, p3;
+	String p[4];	// 4th is just a temporary string
+	int a;
 	cout << face << endl;	// Debug
-	p1 = face.split(' ');
-	p2 = face.split(' ');
-	p3 = face;
-	cout << "p1=" << p1 << " p2=" << p2 << " p3=" << p3 << endl;
+	p[0] = face.split(' ');	// face holds 3 points (vertex and texture indices)
+	p[1] = face.split(' ');	// now face holds 2 points
+	p[2] = face;						// now face holds the last point
+//	cout << "p1=" << p[0] << " p2=" << p[1] << " p3=" << p[2] << endl;	// Debug
+	for(a = 0; a < 3; a++)
+	{
+		p[3] = p[a].split('/');	// Store the string version of point index
+		this->vertex[a].pointIndex = atoi(p[3].c_str());
+		this->vertex[a].textureIndex = atoi(p[a].c_str()); // Convert texture index
+		cout << "p" << a + 1 << "= " << this->vertex[a].pointIndex << "/";
+		cout << this->vertex[a].textureIndex << " ";
+	}
+	cout << endl;
 	return;
 }
 
