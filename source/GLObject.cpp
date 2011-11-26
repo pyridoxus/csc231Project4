2eda467efd11203c475b4ac4e3be55141dde5f61
Craig McCulloch <pyridoxus@null.net>
2011-11-26 08:25:36 -0600

    Beginning to parse object input file.

--- source/GLObject.cpp
+++ source/GLObject.cpp
@@ -3,11 +3,34 @@
 GLObject::GLObject(string objFile)
 {
 	char s[256];
+	string st;
+	Polygon *tempPoly;
 	fstream file(objFile.c_str(), fstream::in);
 	while(file.good())
 	{
-		file.getline(s, 256);
-		cout << s << endl;
+		file.getline(s, 256, ' ');	// Parse through file
+		st.assign(s);
+		if(!st.compare("v"))
+		{
+			file.getline(s, 256);
+			st.assign(s);
+			// Send remainder of the string to the vertex handler
+			cout << "Found vertex" << endl;
+		}
+		if(!st.compare("vt"))
+		{
+			file.getline(s, 256);
+			st.assign(s);
+			// Send remainder of the string to the texture coordinate handler
+			cout << "Found texture coordinates" << endl;
+		}
+		if(!st.compare("f"))
+		{
+			file.getline(s, 256);
+			st.assign(s);
+			tempPoly = new Polygon(st);
+			this->mesh.push_back(tempPoly);
+		}
 	}
 	file.close();
 	return;
--- source/Polygon.cpp
+++ source/Polygon.cpp
@@ -1,18 +1,13 @@
-/*
- * Polygon.cpp
- *
- *  Created on: Nov 26, 2011
- *      Author: pyridoxus
- */
-
 #include "Polygon.h"
 
 Polygon::Polygon(string face)
 {
-
+	cout << face << endl;
+	return;
 }
 
 Polygon::~Polygon()
 {
+	cout << "Polygon Destructor" << endl;
 }
 
